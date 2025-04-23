004266e8f88114a80845a289fc994d25882cbe16
Simple Guruji <kumaramit957014@gmail.com>
2025-04-24 03:58:24 +0800

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
+import app from './app'
 import { Config } from './config'
 
-console.log(Config.PORT)
-console.log(Config.NODE_ENV)
+const startServer = () => {
+    const port = Config.PORT
+
+    try {
+        app.listen(port, () =>
+            console.log(`Server is running  on port ${port}.`),
+        )
+    } catch (error) {
+        console.error('Error starting server', error)
+        process.exit(1)
+    }
+}
+
+startServer()
