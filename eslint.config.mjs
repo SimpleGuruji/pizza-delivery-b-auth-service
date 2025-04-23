41e9536bdf30eedc903a90b30481950fb4eb5bbf
Simple Guruji <kumaramit957014@gmail.com>
2025-04-24 03:38:54 +0800

    test lint-staged 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ export default tseslint.config(
             },
         },
         rules: {
-            // 'no-console': 'error',
+            'no-console': 'error',
             'dot-notation': 'error',
         },
     },
